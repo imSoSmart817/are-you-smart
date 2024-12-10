e44b767036077f553ceeac5233f508d902191260
imSoSmart817 <tp3902@pleasantonusd.net>
2024-12-09 20:30:21 -0800

    Update are-you-smart-4th-grader.js
    added more questions

--- are-you-smart-4th-grader.js
+++ are-you-smart-4th-grader.js
@@ -6,12 +6,17 @@ function smart (){
   alert ("hey " + text);
   var answer = prompt("do you want to know if you are smart ?");
   if (answer == "yes") {
-    var answer1 = prompt ("what is 12*3");
-    if (answer1 == "36") {
+    answer = prompt ("what is 12*3");
+    if (answer == "36") {
       alert ("you are smart! Time for the next question");
-      var answer2 = prompt ("27/3");
-      if (answer2 == "9") {
-        alert ("congratulations you completed the quiz 🥳");
+      answer = prompt ("27/3");
+      if (answer == "9") {
+        answer = prompt ("120/60");
+        if(answer == "20") {
+          alert ("congratulations you completed the quiz 🥳");  
+        } else {
+          alert ("sorry that is incorrect");  
+        }
       } else {
         alert ("sorry that is incorrect");
       }
